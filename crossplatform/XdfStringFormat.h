bdcd6df280e0a4414f691f107f19d7c171f6ac19
Demetry Romanowski <demetryromanowski@gmail.com>
2022-08-21 19:52:57 -0400

    Fixed string formatting temporarily, very hacked, only works with hex numbers

--- crossplatform/XdfStringFormat.h
+++ crossplatform/XdfStringFormat.h
@@ -2,21 +2,26 @@
 #define XDFCREATOR_XDFSTRINGFORMAT_H
 
 #ifdef _WIN32
-
 #include <format>
-
 #define format(arg, ...) std::format(arg, __VA_ARGS__)
 #else
 #include <memory>
-template<typename ... Args>
-std::string string_format( const std::string& format, Args ... args )
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+template< typename T >
+std::string NumToHex( T i )
 {
-    int size_s = std::snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    if( size_s <= 0 ){ throw std::runtime_error( "Error during formatting." ); }
-    auto size = static_cast<size_t>( size_s );
-    std::unique_ptr<char[]> buf( new char[ size ] );
-    std::snprintf( buf.get(), size, format.c_str(), args ... );
-    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+    std::stringstream stream;
+    stream << "0x"
+           << std::setfill ('0') << std::setw(sizeof(T))
+           << std::hex << i;
+    return stream.str();
+}
+
+template<typename ... Args>
+std::string string_format(const std::string& format, Args ... args) {
+    return NumToHex(args...);
 }
 #define format(arg, ...) string_format(arg, __VA_ARGS__)
 #endif
